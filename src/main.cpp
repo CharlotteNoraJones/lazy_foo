1e901bf2e246271bf3e0c28748cee6b5888ba124
CharlotteNoraJones <charlottenorajones@gmail.com>
2024-09-03 22:35:10 -0500

    Did some stuff I dunno.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,61 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+#include <string>
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+bool init();
+bool loadMedia();
+SDL_Surface* loadSurface(std::string path);
+void close();
+
+enum KeyPressSurfaces {
+  KEY_PRESS_SURFACE_DEFAULT,
+  KEY_PRESS_SURFACE_UP,
+  KEY_PRESS_SURFACE_DOWN,
+  KEY_PRESS_SURFACE_LEFT,
+  KEY_PRESS_SURFACE_RIGHT,
+  KEY_PRESS_SURFACE_TOTAL
+};
+
 SDL_Window* gWindow = nullptr;
 SDL_Surface* gScreenSurface = nullptr;
-SDL_Surface* gHelloWorld = nullptr;
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+SDL_Surface* gCurrentSurfaces = nullptr;
+
+int main(int argc, char* args[]) {
+  // These parameters are necessary for SDL2 to function, but won't be used in
+  // our code. The are cast to void to avoid compiler warnings about unused
+  // variables
+  (void)argc;
+  (void)args;
+
+  if (!init()) {
+    printf("Failed to initialize!\n");
+  } else {
+    if (!loadMedia()) {
+      printf("Failed to load media!\n");
+    } else {
+      SDL_BlitSurface(gXOut, nullptr, gScreenSurface, nullptr);
+      SDL_UpdateWindowSurface(gWindow);
+    }
+
+    SDL_Event e;
+    bool quit{false};
+    while (!quit) {
+      while (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT) {
+          quit = true;
+        }
+      }
+    }
+  }
+
+  close();
+  return 0;
+}
 
 bool init() {
   bool success{true};
@@ -31,13 +80,30 @@ bool init() {
   return success;
 }
 
-bool loadMedia() {
-  bool success{true};
+SDL_Surface* loadSurface(std::string path) {
+  SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+  if (loadedSurface == nullptr) {
+    printf("Unable to load image %s! SDL Error: %s\n", path.c_str(),
+           SDL_GetError());
+  }
 
-  gHelloWorld = SDL_LoadBMP("../resources/hello_world.bmp");
-  if (gHelloWorld == nullptr) {
-    printf("Unable to load image %s! SDL Error: %s\n",
-           "../resources/hello_world.png", SDL_GetError());
+  return loadedSurface;
+}
+
+bool loadMedia() {
+  bool success = true;
+
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] =
+      loadSurface("../resources/press.bmp");
+  if (gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == nullptr) {
+    printf("Failed to load default image!\n");
+    success = false;
+  }
+
+  gKeyPressedSurfaces[KEY_PRESS_SURFACE_UP] =
+      loadSurface("../resources/up.bmp");
+  if (gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == nullptr) {
+    printf("Failed to load up image!\n");
     success = false;
   }
 
@@ -45,43 +111,11 @@ bool loadMedia() {
 }
 
 void close() {
-  SDL_FreeSurface(gHelloWorld);
-  gHelloWorld = nullptr;
+  SDL_FreeSurface(gXOut);
+  gXOut = nullptr;
 
   SDL_DestroyWindow(gWindow);
   gWindow = nullptr;
 
   SDL_Quit();
-}
-
-int main(int argc, char* args[]) {
-  // These parameters are necessary for SDL2 to function, but won't be used in
-  // our code. The are cast to void to avoid compiler warnings about unused
-  // variables
-  (void)argc;
-  (void)args;
-
-  if (!init()) {
-    printf("Failed to initialize!\n");
-  } else {
-    if (!loadMedia()) {
-      printf("Failed to load media!\n");
-    } else {
-      SDL_BlitSurface(gHelloWorld, nullptr, gScreenSurface, nullptr);
-      SDL_UpdateWindowSurface(gWindow);
-    }
-
-    SDL_Event e;
-    bool quit{false};
-    while (!quit) {
-      while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_QUIT) {
-          quit = true;
-        }
-      }
-    }
-  }
-
-  close();
-  return 0;
 }
